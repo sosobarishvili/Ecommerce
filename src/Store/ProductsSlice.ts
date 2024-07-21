22b7f3e6e8f4cbd07848634ef9d006d003917e86
Soso Barishvili <barishvili.soso@gmail.com>
2024-07-21 13:44:16 +0400

    link testing

--- src/Store/ProductsSlice.ts
+++ src/Store/ProductsSlice.ts
@@ -18,7 +18,6 @@ const initialState: InitialState = {
 export const fetchProducts = async () => {
 
   return await axios.get('https://sosobarishvili.github.io/Ecommerce/db.json').then(({ data }) => {
-    console.log(data)
     return data
   })
 
